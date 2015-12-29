adfda2e1d96bd79f8432e376bbb2ed6168236519
Tino Kiviharju <tino.kiviharju@haltu.fi>
2015-12-29 18:10:21 -0400

    pep8 fixes

--- selector/views/invitee.py
+++ selector/views/invitee.py
@@ -1,19 +1,19 @@
 # -*- encoding: utf-8 -*-
 
 # The MIT License (MIT)
-# 
+#
 # Copyright (c) 2015 Haltu Oy, http://haltu.fi
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,19 +21,15 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-#
 
 
 import logging
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse_lazy
 from django.utils.encoding import force_text
 from django.utils.decorators import method_decorator
 from django.views.generic import View, TemplateView, FormView
 from django.contrib.auth.decorators import login_required
-from django.contrib.sites.models import get_current_site
-from selector.models import RegisterToken
 from selector.forms import RegisterForm
 
 LOG = logging.getLogger(__name__)
@@ -87,7 +83,7 @@ class RegisterUserView(View):
         invitee = {
           'eppn': request.META.get('HTTP_AUTHNID', None),
           'auth_method': request.META.get('HTTP_AUTHENTICATOR', None),
-          }
+        }
         if token.register(token.user, **invitee):
           print '3'
           return HttpResponseRedirect(self.success_url)
@@ -102,4 +98,5 @@ class RegisterFailedView(ClearSessionMixin, TemplateView):
   template_name = 'register_failed.html'
 
 
+# vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2
 
